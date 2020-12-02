0de046e3f630415153057f0828c5211111b2eb3a
SickanDev <44079953+SickanDev@users.noreply.github.com>
2020-12-02 23:28:03 +0100

    Day 4 2019

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,15 +1,61 @@
 import math
+import re
 from tools.input import Input, strToArr, strToInt, intToStr, arrToStr
 
-i = Input(2019, 4).getData()
+i = [372304, 847060]
 
 
 def PartOne(info):
-    return 0
+    total = 0
+    for i in range(info[0], info[1]):
+        arr = list(str(i))
+        yes = 0
+        yes2 = 0
+
+        for l in range(1, len(arr)):
+            l = int(l)
+            if(int(arr[l-1]) <= int(arr[l]) and yes2 != -1):
+                yes2 += 1
+            else:
+                yes2 = -1
+
+            if(int(arr[l-1]) == int(arr[l])):
+                yes += 1
+
+        if(yes > 0 and yes2 > 0):
+            total += 1
+
+        yes = 0
+        yes2 = 0
+        arr = []
+
+    return total
 
 
 def PartTwo(info):
-    return 0
+    total = 0
+    for i in range(info[0], info[1]):
+        arr = list(str(i))
+        yes2 = 0
+
+        for l in range(1, len(arr)):
+            l = int(l)
+            if(int(arr[l-1]) <= int(arr[l]) and yes2 != -1):
+                yes2 += 1
+            else:
+                yes2 = -1
+
+        test = re.findall(r"((^)|(.))((?(3)(?!\1).|.))\4(?!\4)", str(i))
+
+        if(test and yes2 > 0):
+            print(i)
+            print(test)
+            total += 1
+
+        yes2 = 0
+        arr = []
+
+    return total
 
 
 print(i)
