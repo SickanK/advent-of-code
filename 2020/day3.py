fc212ee0c2c51391a3950dc8cce8bbe04d59a8f3
SickanDev <44079953+SickanDev@users.noreply.github.com>
2020-12-03 07:33:36 +0100

    Uncomplete 3rd day

--- 2020/day3.py
+++ 2020/day3.py
@@ -8,7 +8,31 @@ i = Input(2020, 3).getData()
 def PartOne(info):
     info = strToArr(info)
 
-    return 0
+    tempTrees = []
+    trees = []
+    for i in range(0, len(info)):
+        for j in range(0, len(info[i])):
+            if(info[i][j] == '#'):
+                tempTrees.append(j)
+            else:
+                tempTrees.append(0)
+
+        trees.append(tempTrees)
+        tempTrees = []
+
+    i = 0
+    total = 0
+    info = info[0:len(info):2]
+    for t in list(info):
+        if(i >= 31):
+            i -= 31
+
+        if(t[i] == '#'):
+            total += 1
+
+        i += 1
+
+    return total
 
 
 def PartTwo(info):
@@ -17,6 +41,6 @@ def PartTwo(info):
     return 0
 
 
-print(i)
+# print(i)
 print(PartOne(i))
 print(PartTwo(i))
