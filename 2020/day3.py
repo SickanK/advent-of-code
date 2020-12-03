c0a48a312e7f964e3bf1656019227cd5599b9b8b
SickanDev <44079953+SickanDev@users.noreply.github.com>
2020-12-03 09:10:57 +0100

    Fixed 3rd day solution

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,44 +1,33 @@
-import math
+
+import math 
 import re
 from tools.input import Input, strToArr, strToInt, intToStr, arrToStr
 
 i = Input(2020, 3).getData()
 
-
-def PartOne(info):
+def PartOne(info, right = 3, down = 1):
     info = strToArr(info)
 
-    tempTrees = []
-    trees = []
-    for i in range(0, len(info)):
-        for j in range(0, len(info[i])):
-            if(info[i][j] == '#'):
-                tempTrees.append(j)
-            else:
-                tempTrees.append(0)
-
-        trees.append(tempTrees)
-        tempTrees = []
-
-    i = 0
-    total = 0
-    info = info[0:len(info):2]
+    i, total, info = 0, 0, info[0:len(info):down]
     for t in list(info):
-        if(i >= 31):
-            i -= 31
+        if(i >= len(t)):
+            i -= len(t)
 
         if(t[i] == '#'):
             total += 1
-
-        i += 1
+        i += right
 
     return total
 
 
 def PartTwo(info):
-    info = strToArr(info)
+    t1 = PartOne(info, 1, 1)
+    t2 = PartOne(info, 3, 1)
+    t3 = PartOne(info, 5, 1)
+    t4 = PartOne(info, 7, 1)
+    t5 = PartOne(info, 1, 2)
 
-    return 0
+    return t1 * t2 * t3 * t4 * t5
 
 
 # print(i)
---
+++ 2020/day4.py
@@ -0,0 +1,20 @@
+
+import math
+import re
+from tools.input import Input, strToArr, strToInt, intToStr, arrToStr
+
+i = Input(2020, 4).getData()
+
+def PartOne(info, right = 3, down = 1):
+
+    return 0
+
+
+def PartTwo(info):
+
+    return 0
+
+
+# print(i)
+print(PartOne(i))
+print(PartTwo(i))
