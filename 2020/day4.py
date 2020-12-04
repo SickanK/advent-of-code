5ae1029d32114dfa3ec5f83c7d2c3e2fcb4f4298
SickanDev <44079953+SickanDev@users.noreply.github.com>
2020-12-04 06:03:21 +0100

    Test

--- 2020/day4.py
+++ 2020/day4.py
@@ -7,19 +7,8 @@ i = Input(2020, 3).getData()
 
 def PartOne(info):
     info = strToArr(info)
-    i = 0
-    total = 0
-    info = info[0:len(info):2]
-    for t in list(info):
-        if(i >= 31):
-            i -= 31
 
-        if(t[i] == '#'):
-            total += 1
-
-        i += 1
-
-    return total
+    return 0
 
 
 def PartTwo(info):
