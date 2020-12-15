1def9513e5bfc30c059aad35d3719c289442860d
SickanDev <44079953+SickanDev@users.noreply.github.com>
2020-12-15 08:19:04 +0100

    Day 15 solution

--- 2020/day15.py
+++ 2020/day15.py
@@ -7,28 +7,55 @@ i = Input(2020, 15).getData()
 # i = Input(2020, 15).getFromExample()
 
 
-def PartOne(info):
+def PartOne(info, num=2020):
     # noParseInfo = info
     info = strToArr(info)
+    lastTurns = {}
+    lastNum = int(info[0])
+    firstNum = True
 
-    return 0
+    for i in range(len(info)):
+        s = str(info[i])
+        lastTurns[s] = [i+1]
+        lastNum = str(s)
+
+    for i in range(len(info) + 1, num+1):
+        if(firstNum):
+            lastNum = "0"
+            firstNum = False
+            if(lastTurns.get(lastNum) is None):
+                lastTurns[lastNum] = [i]
+            else:
+                turnBeforeLast = lastTurns[lastNum][-1]
+                lastTurns[lastNum] = [int(turnBeforeLast), i]
+        else:
+            turnBeforeLast = lastTurns[lastNum][-2]
+            turnLast = lastTurns[lastNum][-1]
+
+            lastNum = str(int(turnLast) - int(turnBeforeLast))
+
+            if(lastTurns.get(lastNum) is None):
+                lastTurns[lastNum] = [i]
+                firstNum = True
+            else:
+                turnBeforeLast = lastTurns[lastNum][-1]
+                lastTurns[lastNum] = [int(turnBeforeLast), i]
+
+    return lastNum
 
 
 def PartTwo(info):
-    # noParseInfo = info
-    info = strToArr(info)
-
-    return 0
+    return PartOne(info, 30000000)
 
 
 print("Input:\n", i)
 # print("--------Results--------")
 print("Part 1:", PartOne(i))
-# print("Part 2:", PartTwo(i))
+print("Part 2:", PartTwo(i))
 
 # Runtime
-# print("\n--------Runtime--------")
-# print("Part 1:", str((timeit.timeit(
-#     f"PartOne(i)", globals=locals(), number=1000))) + "ms")
-# print("Part 2:", str((timeit.timeit(
-#     f"PartTwo(i)", globals=locals(), number=1000))) + "ms")
+print("\n--------Runtime--------")
+print("Part 1:", str((timeit.timeit(
+    f"PartOne(i)", globals=locals(), number=100)*10)) + "ms")
+print("Part 2:", str((timeit.timeit(
+    f"PartTwo(i)", globals=locals(), number=1)*1000)) + "ms")
